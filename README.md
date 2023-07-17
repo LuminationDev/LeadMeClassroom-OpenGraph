695ab585c0804a0daac9fe030dc5bf2ab0dbb9b0
dkearns-lumination <dkearns@lumination.com.au>
2023-07-17 10:31:33 +0930

    Cloud function to upload OpenGraph images to storage bucket

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# LeadMeClassroom-OpenGraph" 
+# LeadMeClassroom-OpenGraph
+## Setting environment variables for running locally
+Using powershell run `$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\...firebaseconfig.json"`
+
+## Running
+`npm install`
+
+`npm run serve`
---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import {credential} from "firebase-admin";
+import { getStorage } from "firebase-admin/storage";
+import applicationDefault = credential.applicationDefault;
+const axios = require('axios')
+const ogs = require('open-graph-scraper')
+
+export const fetchPreviewImage = functions.https.onRequest((request, response) => {
+  if (request.method !== "POST") {
+    response.status(405);
+    response.send();
+    return;
+  }
+  if (!JSON.parse(request.body).url) {
+    response.status(422);
+    response.send();
+    return;
+  }
+  if (!JSON.parse(request.body).token) {
+    response.status(422);
+    response.send();
+    return;
+  }
+  if (admin.apps.length < 1) {
+    admin.initializeApp({
+      credential: applicationDefault(),
+    });
+  }
+
+  admin.auth().verifyIdToken(JSON.parse(request.body).token).then((result) => {
+    const url = JSON.parse(request.body).url;
+    const options = { url }
+    ogs(options).then((data: any) => {
+      const { result } = data;
+      const bucket = getStorage().bucket("browserextension-bc94e.appspot.com");
+      if (!result.ogImage ||
+        result.ogImage.length < 1 ||
+        !result.ogImage[0].url) {
+        response.status(400);
+        response.send();
+        return;
+      }
+      if (result.ogImage[0].type !== 'image/jpeg') {
+        response.status(400);
+        response.send();
+        return;
+      }
+      if (result.ogImage[0].url) {
+        axios({
+          url: result.ogImage[0].url,
+          method: 'GET',
+          responseType: 'stream'
+        }).then((responseStream: any) => {
+          const file = bucket.file('preview_images/' + url.replaceAll('/', '-') + ".jpg")
+          responseStream.data.pipe(file.createWriteStream()).on('finish', () => {
+            response.status(200);
+            response.send();
+            return;
+          })
+        }).catch((error: any) => {
+            response.status(401);
+            response.send();
+            return;
+        })
+      }
+    }).catch((error: any) => {
+      response.status(400);
+      response.send();
+      return;
+    })
+  }).catch(() => {
+    response.status(401);
+    response.send();
+    return;
+  });
+});
